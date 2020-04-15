44fb69c1aa77209b97b14129d7aa3af0dc4468fb
Richard Clifford <richard.clifford@soluis.com>
2020-04-15 20:14:12 +0100

    nested columns in row - needed expanded as parent

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,10 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Widget buildKey({int soundNumber, MaterialColor color}) {
+  Widget buildKey({int soundNumber, MaterialColor color, int shade = 500}) {
     return Expanded(
       child: FlatButton(
-        color: color,
+        color: color[shade],
         onPressed: () {
           playSound(soundNumber);
         },
@@ -24,18 +24,53 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.green,
         body: SafeArea(
-          child: Column(
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(soundNumber: 1, color: Colors.red),
-              buildKey(soundNumber: 2, color: Colors.orange),
-              buildKey(soundNumber: 3, color: Colors.yellow),
-              buildKey(soundNumber: 4, color: Colors.green),
-              buildKey(soundNumber: 5, color: Colors.teal),
-              buildKey(soundNumber: 6, color: Colors.blue),
-              buildKey(soundNumber: 7, color: Colors.purple),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    buildKey(soundNumber: 1, color: Colors.red),
+                    buildKey(soundNumber: 2, color: Colors.orange),
+                    buildKey(soundNumber: 3, color: Colors.yellow),
+                    buildKey(soundNumber: 4, color: Colors.green),
+                    buildKey(soundNumber: 5, color: Colors.teal),
+                    buildKey(soundNumber: 6, color: Colors.blue),
+                    buildKey(soundNumber: 7, color: Colors.purple),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    buildKey(soundNumber: 1, color: Colors.red, shade: 300),
+                    buildKey(soundNumber: 2, color: Colors.orange, shade: 300),
+                    buildKey(soundNumber: 3, color: Colors.yellow, shade: 300),
+                    buildKey(soundNumber: 4, color: Colors.green, shade: 300),
+                    buildKey(soundNumber: 5, color: Colors.teal, shade: 300),
+                    buildKey(soundNumber: 6, color: Colors.blue, shade: 300),
+                    buildKey(soundNumber: 7, color: Colors.purple, shade: 300),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    buildKey(soundNumber: 1, color: Colors.red, shade: 100),
+                    buildKey(soundNumber: 2, color: Colors.orange, shade: 100),
+                    buildKey(soundNumber: 3, color: Colors.yellow, shade: 100),
+                    buildKey(soundNumber: 4, color: Colors.green, shade: 100),
+                    buildKey(soundNumber: 5, color: Colors.teal, shade: 100),
+                    buildKey(soundNumber: 6, color: Colors.blue, shade: 100),
+                    buildKey(soundNumber: 7, color: Colors.purple, shade: 100),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
